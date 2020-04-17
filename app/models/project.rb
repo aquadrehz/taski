b4d13996378961d9605dd25aebeb6709c560a8fe
shumman.sue <shumnan.sue@ascendcorp.com>
2020-04-17 15:40:14 +0700

    Apply model scope

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ApplicationRecord
   # Custom scopes
+  scope :almost_completed, -> {
+    where('percent_completed > 75.0')
+  }
+  scope :still_working, -> {
+    where('percent_completed <= 75.0')
+  }
   # Add defaults
   # Integrate validation
   # Integrate callback
   # Integrate database relationships
-
 end

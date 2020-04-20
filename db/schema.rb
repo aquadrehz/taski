c824aa268e8be59b7f1af91617b6e615334e2d54
shumman.sue <shumnan.sue@ascendcorp.com>
2020-04-20 11:39:55 +0700

    Migrate DB rails g migration add_task_file_to_tasks task_file:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_093439) do
+ActiveRecord::Schema.define(version: 2020_04_20_043848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_093439) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed"
+    t.text "task_file"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 

aaf9aa1d7d1fa533a8b11bbc2f496ab248e15b59
shumman.sue <shumnan.sue@ascendcorp.com>
2020-04-20 11:20:51 +0700

    Initial File uploader for Tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def tasks
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/tasks'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
   get "error", to: "pages#error"
